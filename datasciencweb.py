2cb03cd21440ee6bdc32536b283c5ec7aa62902e
schoLigth <lusiajulisilaban@gmail.com>
2025-10-16 15:36:09 +0800

    nambah file datasciencweb

---
+++ datasciencweb.py
@@ -0,0 +1,15 @@
+# Import Python Libraries
+import pandas as pd
+import folium # peta interaktif (zoom, klik, hover)
+import geopandas as gpd # data geografis yang mendukung titik, garis, dan poligon
+from folium.features import GeoJSonPopup, GeoJsonToolTip # file berbasis JSON untuk menyimpan data geografis. Untuk menyimpan batas wilayah, kordinat, dan metadata
+import streamlit as st
+from streamlit_folium import folium_static
+
+@st.cache
+def read_csv(path):
+    return pd.read_csv(path, compression='gzip', sep='t', quotechar='"')
+
+
+st.title("Data Prepartion to Gogjson Data")
+st.write("Data science for beginners")
